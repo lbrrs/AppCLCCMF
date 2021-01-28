658d9637ecb1adedea03d892061f760cdad039f6
Lucas Barrois <barroislucas@outlook.fr>
2021-01-28 19:20:26 +0100

    update creation client

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("#plus").click(function() {
+  $(this).nextAll("*:hidden").first().toggle('normal');
+  $(this).nextAll("*:hidden").first().toggle('normal');
+  });
